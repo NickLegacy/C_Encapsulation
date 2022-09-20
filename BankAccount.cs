f98a32ee9d5279ee67bcfa1b30564a7ef79fcf03
NickLegacy <nicklegacy13@gmail.com>
2022-09-20 00:02:04 -0400

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double _balance;
+
+        public void depost(double amount)
+        {
+            Console.WriteLine($"Deposit of {amount,0:c} to account");
+            _balance = amount;
+        }
+
+        public double GetBalance()
+        {
+        return _balance;
+        }
+
+
+
+    }
+}
